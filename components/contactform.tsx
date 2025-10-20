053412e0998a9b7dfc19b12feaff58cc3ea6aa30
Swastik Yadav <swastikb380@gmail.com>
2025-10-20 09:02:45 +0530

    move contact form into separate component

---
+++ components/contactform.tsx
@@ -0,0 +1,40 @@
+function ContactForm() {
+  return (
+    <div className="w-full md:w-5/12 bg-white rounded-lg text-gray-800 p-12 text-center">
+      <h4 className="text-3xl">Get In Touch</h4>
+      <form className="flex flex-col">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          className="bg-gray-200 rounded w-full p-3 my-4"
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Email Address"
+          className="bg-gray-200 rounded w-full p-3 my-4"
+        />
+        <input
+          type="tel"
+          name="phone"
+          placeholder="Phone Number"
+          className="bg-gray-200 rounded w-full p-3 my-4"
+        />
+        <textarea
+          name="message"
+          placeholder="Message"
+          className="bg-gray-200 rounded w-full p-3 my-4"
+        />
+
+        <input
+          type="submit"
+          value="Send"
+          className="bg-brand text-white rounded cursor-pointer w-full p-3 my-4"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
